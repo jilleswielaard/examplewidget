5f5f5e912d760873247de4f40964047f00e903e2
Jilles Wielaard <jilles.wielaard@kpn.com>
2023-04-05 09:00:05 +0200

    implements switch component

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,3 +1,12 @@
 <template>
   <h1 class="text-3xl font-bold underline">This the welcome component</h1>
+  <hui-switch v-model="enabled" />
+  <p>{{ enabled }}</p>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import HuiSwitch from '@/components/HuiSwitch.vue';
+
+const enabled = ref(false);
+</script>
