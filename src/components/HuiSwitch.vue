732480953f81ce52a79d4221fb488884a9a9e9a6
Jilles Wielaard <jilles.wielaard@kpn.com>
2023-04-05 08:59:41 +0200

    created switch component

---
+++ src/components/HuiSwitch.vue
@@ -0,0 +1,17 @@
+<template>
+  <Switch v-model="enabled" as="template" v-slot="{ checked }">
+    <button class="relative inline-flex h-6 w-11 items-center rounded-full" :class="checked ? 'bg-primary' : 'bg-inactive'">
+      <span
+        :class="checked ? 'translate-x-6' : 'translate-x-1'"
+        class="inline-block h-4 w-4 transform rounded-full bg-white transition"
+      />
+    </button>
+  </Switch>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Switch } from '@headlessui/vue';
+
+const enabled = ref(false);
+</script>
